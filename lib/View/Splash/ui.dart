0135a2b3daca759a826b15ab1498b26be8d8a8a9
Niponpal <niponpal52@gmail.com>
2026-02-05 10:36:47 +0600

    Init Project 🚀🚀🚑

---
+++ lib/View/Splash/ui.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(38.0),
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/Splash.png"),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:grocery/View/Splash/ui.dart';
+
+void main (){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    )
+  );
+}
